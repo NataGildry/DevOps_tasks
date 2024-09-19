ccdb5a841f2736d47bf2ae7dab701f49b3a92b44
NPodpriatova <natalia.podpryatova@gmail.com>
2024-09-20 00:09:12 +0200

    docs: remove extra cpmments

--- Lecture_6/custom_service_script.sh
+++ Lecture_6/custom_service_script.sh
@@ -2,10 +2,8 @@
 
 echo "Creating systemd service and timer..." | tee -a /var/log/provision.log
       
-# Create directory for the script
 sudo mkdir -p /usr/local/bin/my_script
 
-# Logging script
 echo '#!/bin/bash' | sudo tee /usr/local/bin/my_script/log_time.sh
 echo 'echo "Current date and time: $(date)" >> /var/log/my_script.log' | sudo tee -a /usr/local/bin/my_script/log_time.sh
 
@@ -32,6 +30,5 @@ echo 'Unit=log_time.service' | sudo tee -a /etc/systemd/system/log_time.timer
 echo '[Install]' | sudo tee -a /etc/systemd/system/log_time.timer
 echo 'WantedBy=timers.target' | sudo tee -a /etc/systemd/system/log_time.timer
 
-# Enable and start the timer
 sudo systemctl enable log_time.timer
 sudo systemctl start log_time.timer
