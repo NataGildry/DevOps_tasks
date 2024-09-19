07941f05bc5f27060a9dd6a591a2973dd3f5581d
NPodpriatova <natalia.podpryatova@gmail.com>
2024-09-19 23:33:31 +0200

    feat: use script from the file

--- Lecture_6/README.md
+++ Lecture_6/README.md
@@ -4,8 +4,30 @@ This project provides a step-by-step guide to set up an Nginx web server, config
 ## Prerequisites
 
 Before starting, make sure you have the following:
+- [Vagrant](https://www.vagrantup.com/downloads)
+- [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
 - Ubuntu-based Linux distribution.
 - Sudo/root access to install and configure software.
+
+## Setup Instructions
+
+1. Clone this repository to your local machine.
+    ```bash
+    git clone https://github.com/NataGildry/DevOps_tasks.git
+    cd DevOps_tasks/Lecture_4
+    ```
+
+2. Ensure the shared folder path on your host machine is correctly set in the `Vagrantfile`.
+
+3. Run the following command to start the virtual machine:
+    ``` bash
+     vagrant up --provider=virtualbox
+    ```
+
+4. After the VM is up and running, you can SSH into the VM using:
+    ``` bash
+     vagrant ssh
+    ```
 
 ## Steps Overview
 
---
+++ Lecture_6/install_nginx_script.sh
@@ -0,0 +1,33 @@
+LOGFILE="/var/log/nginx_install.log"
+echo "Updating package list..." | tee -a $LOGFILE
+sudo apt-get update | tee -a $LOGFILE
+  
+echo "Installing Nginx from the official repository..." | tee -a $LOGFILE
+sudo apt-get install -y nginx | tee -a $LOGFILE
+  
+echo "Checking Nginx installation source..." | tee -a $LOGFILE
+ apt-cache policy nginx | tee -a $LOGFILE
+  
+echo "Adding PPA repository for Nginx..." | tee -a $LOGFILE
+sudo add-apt-repository -y ppa:nginx/stable | tee -a $LOGFILE
+sudo apt-get update | tee -a $LOGFILE
+  
+echo "Installing Nginx from PPA if available..." | tee -a $LOGFILE
+sudo apt-get install -y nginx | tee -a $LOGFILE
+  
+echo "Checking Nginx installation source after PPA installation..." | tee -a $LOGFILE
+apt-cache policy nginx | tee -a $LOGFILE
+  
+echo "Reverting to the official version of Nginx using ppa-purge..." | tee -a $LOGFILE
+sudo apt-get install -y ppa-purge | tee -a $LOGFILE
+
+sudo ppa-purge -y ppa:nginx/stable | tee -a $LOGFILE
+sudo apt-get update | tee -a $LOGFILE
+  
+echo "Starting Nginx..." | tee -a $LOGFILE
+sudo systemctl start nginx | tee -a $LOGFILE
+  
+echo "Checking Nginx status..." | tee -a $LOGFILE
+sudo systemctl status nginx | tee -a $LOGFILE
+  
+echo "Script execution completed!" | tee -a $LOGFILE
